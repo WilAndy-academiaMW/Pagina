17b0824fdb696ec72538c384836a4e5fef98643a
Elhombreydios23 <digitalypotente50p@gmail.com>
2024-09-01 19:39:15 -0400

    Descripción de los cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css';
-
+import { Nav } from './assets/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Adivinanza } from './juegos/adivinanza';
+import { Elexplorador } from './ElExplorador/Elexplorador';
+import { ElSaber } from './El saber/elSaber';
+import { Inicio } from './index/Inicio';
 
 
 
 function App() {
   return (
- 
-   <>
-   hola ratas
-   </>
+ <BrowserRouter>
+     <Routes> 
+      <Route path='/' element={<Inicio/>} />
+  {/*     <Route path='/Nav' element={<Nav/>}/>
+      <Route path='/Elexplorador' element={<Elexplorador/>}/>
+      <Route path='/Adivinanza' element={<Adivinanza/>}/>
+      <Route path='/elSaber' element={<ElSaber/>}/>
+      */}
+     </Routes>  
+  </BrowserRouter>
+   
   
   );
 }
